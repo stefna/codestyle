184131dc4dc517d99f2300d231ec1b2f78e58839
Andreas Sundqvist <andreas@stefna.is>
2018-09-12 16:24:34 +0200

    Fix issue with composer json_encode

--- bin/install-composer-scripts.php
+++ bin/install-composer-scripts.php
@@ -4,19 +4,19 @@ if (!file_exists('composer.json')) {
 	die('No composer.json found');
 }
 
-$composer = json_decode(file_get_contents('composer.json'), true);
+$composer = json_decode(file_get_contents('composer.json'));
 
-if (isset($composer['scripts']['check'])) {
+if (isset($composer->scripts->check)) {
 	die('Composer file already have scripts check');
 }
 
-if (!isset($composer['scripts'])) {
-	$composer['scripts'] = [];
+if (!isset($composer->scripts)) {
+	$composer->scripts = new stdClass();
 }
 
 $args = '--standard=vendor/stefna/codestyle/library.xml src/ tests/';
 
-$composer['scripts']['check'] = './vendor/bin/phpcs ' . $args;
-$composer['scripts']['fix'] = './vendor/bin/phpcbf ' . $args;
+$composer->scripts->check = './vendor/bin/phpcs ' . $args;
+$composer->scripts->fix = './vendor/bin/phpcbf ' . $args;
 
-file_put_contents('composer.json', json_encode($composer));
+file_put_contents('composer.json', json_encode($composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
