db2e9ce95b018cb7cef9977ec9b00bd4ff76c7af
DColt <dev@dcolt.org>
2025-12-15 11:38:40 +0800

    [refactor] Use TokenCollection in BracketPlacementSniff

--- src/Stefna/Sniffs/ControlStructures/BracketPlacementSniff.php
+++ src/Stefna/Sniffs/ControlStructures/BracketPlacementSniff.php
@@ -5,6 +5,7 @@ namespace Stefna\Sniffs\ControlStructures;
 use PHP_CodeSniffer\Files\File;
 use PHP_CodeSniffer\Sniffs\Sniff;
 use PHP_CodeSniffer\Util\Tokens;
+use Stefna\Utils\TokenCollection;
 
 final class BracketPlacementSniff implements Sniff
 {
@@ -27,15 +28,11 @@ final class BracketPlacementSniff implements Sniff
 
 	public function process(File $phpcsFile, int $stackPtr): void
 	{
-		$tokens = $phpcsFile->getTokens();
-
-		$currentToken = $tokens[$stackPtr];
-		$currentLine = $currentToken['line'];
+		$tokens = new TokenCollection($phpcsFile->getTokens());
 
 		$previousCloseBracketPtr = $phpcsFile->findPrevious(T_CLOSE_CURLY_BRACKET, $stackPtr - 1);
 		if ($previousCloseBracketPtr) {
-			$bracketToken = $tokens[$previousCloseBracketPtr];
-			if ($currentLine === $bracketToken['line']) {
+			if ($tokens->sameLine($stackPtr, $previousCloseBracketPtr)) {
 				$error = 'Closing brace must be on a line by itself';
 				$fix = $phpcsFile->addFixableError($error, $previousCloseBracketPtr, 'BracketBeforeControlStatement');
 				if ($fix) {
@@ -46,8 +43,7 @@ final class BracketPlacementSniff implements Sniff
 
 		$nextCommentPtr = $phpcsFile->findNext(Tokens::COMMENT_TOKENS, $stackPtr);
 		if ($nextCommentPtr) {
-			$commentToken = $tokens[$nextCommentPtr];
-			if ($currentLine === $commentToken['line']) {
+			if ($tokens->sameLine($stackPtr, $nextCommentPtr)) {
 				$error = 'Can\'t have comment on the same line as control statement';
 				$fix = $phpcsFile->addError($error, $stackPtr, 'CommentAfterControlStatement');
 				if ($fix) {
